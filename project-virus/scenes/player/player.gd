169e41407511c94a061bdf9d23b1f5f35ec49353
Ryan Barker <35574294+Ryanbarker0@users.noreply.github.com>
2025-01-17 23:10:24 +0000

    Initial project setup (#1)

---
+++ project-virus/scenes/player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+class_name Player
+
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+    # Get the input direction and handle the movement.
+    var direction = Vector2(
+        Input.get_axis("ui_left", "ui_right"),
+        Input.get_axis("ui_up", "ui_down")
+    ).normalized()
+
+    velocity = direction * SPEED
+
+    move_and_slide()
