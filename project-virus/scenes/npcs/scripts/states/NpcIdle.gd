28f561b2318ddf2f61a6e2a277cf365a8a6f5e9d
Ryan Barker <35574294+Ryanbarker0@users.noreply.github.com>
2025-01-18 21:10:28 +0000

    Update hit boxes (#14)

--- project-virus/scenes/npcs/scripts/states/NpcIdle.gd
+++ project-virus/scenes/npcs/scripts/states/NpcIdle.gd
@@ -3,7 +3,6 @@ class_name NpcIdle
 
 @export var npc: CharacterBody2D
 @export var move_speed: float = 50.0
-@export var infection_area: Area2D
 @export var proximity_area: Area2D
 
 var move_direction: Vector2
@@ -11,8 +10,7 @@ var wonder_time: float
 
 func Enter():
 	# Subscribe to infection area signal
-
-	proximity_area.body_entered.connect(on_proximity_area_body_entered)
+	proximity_area.body_entered.connect(on_proximity_area_entered)
 	randomize_wonder()
 
 func Update(delta: float) -> void:
@@ -31,8 +29,9 @@ func randomize_wonder():
 	wonder_time = randf_range(1, 3)
 
 # Signals
-func on_proximity_area_body_entered(body:Node2D):
+func on_proximity_area_entered(body:Node2D):
 	if body is Player:
 		Transitioned.emit(self, "NpcFlee")
-		proximity_area.body_entered.disconnect(on_proximity_area_body_entered)
 		# The NPC is fleeing, so we no longer need to listen to the proximity area signal
+		proximity_area.body_entered.disconnect(on_proximity_area_entered)
+		
