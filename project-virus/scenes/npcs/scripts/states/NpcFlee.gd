58cc8c46f98632199d25124e8ec52c8a1788bd4f
Ryan <ryangeorgebarker0@gmail.com>
2025-01-18 21:04:49 +0000

    update collisions layers + implement hit/hurtbox

--- project-virus/scenes/npcs/scripts/states/NpcFlee.gd
+++ project-virus/scenes/npcs/scripts/states/NpcFlee.gd
@@ -13,7 +13,7 @@ var player: CharacterBody2D
 
 
 func Enter():
-	infection_area.body_entered.connect(on_infection_area_body_entered)
+	infection_area.hurt.connect(on_infection_hurtbox_entered)
 	proximity_area.body_exited.connect(on_proximity_area_body_exited)
 	# use the proximity area to get the player's position
 	var bodies_in_proximity = proximity_area.get_overlapping_bodies()
@@ -38,16 +38,18 @@ func Physics_Update(_delta: float):
 
 
 # Signals
-func on_infection_area_body_entered(body:Node2D):
-	if body is Player:
-		Transitioned.emit(self, "NpcInfected")
-		# The NPC is infected, so we no longer need to listen to the infection or proximity area signal
-		infection_area.body_entered.disconnect(on_infection_area_body_entered)
-		proximity_area.body_exited.disconnect(on_proximity_area_body_exited)
+func on_infection_hurtbox_entered(area: Area2D):
+	if area is HitboxComponent:
+		var _parent = area.get_parent()
+		if _parent is Player:
+			Transitioned.emit(self, "NpcInfected")
+			# The NPC is infected, so we no longer need to listen to the infection or proximity area signal
+			infection_area.hurt.disconnect(on_infection_hurtbox_entered)
+			proximity_area.body_exited.disconnect(on_proximity_area_body_exited)
 
 func on_proximity_area_body_exited(body:Node2D):
 	if body is Player:
 		Transitioned.emit(self, "NpcIdle")
 		# The NPC is no longer fleeing, so we no longer need to listen to the proximity area signal
-		infection_area.body_entered.disconnect(on_infection_area_body_entered)
+		infection_area.hurt.disconnect(on_infection_hurtbox_entered)
 		proximity_area.body_exited.disconnect(on_proximity_area_body_exited)
