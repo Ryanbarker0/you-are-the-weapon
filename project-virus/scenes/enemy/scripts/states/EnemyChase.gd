bbe104c881072090cc21d9890a84f1b14f0328ca
Peachy3D <alex.mortimerr@gmail.com>
2025-01-18 16:14:51 +0000

    enemynpc
    Added enemy npc state logic

--- project-virus/scenes/enemy/scripts/states/EnemyChase.gd
+++ project-virus/scenes/enemy/scripts/states/EnemyChase.gd
@@ -1,9 +1,8 @@
 extends State
-class_name NpcFlee
+class_name EnemyChase
 
-@export var npc: CharacterBody2D
+@export var enemy: CharacterBody2D
 @export var move_speed: float = 80.0
-@export var infection_area: Area2D
 @export var proximity_area: Area2D
 
 var move_direction: Vector2
@@ -13,7 +12,6 @@ var player: CharacterBody2D
 
 
 func Enter():
-	infection_area.body_entered.connect(on_infection_area_body_entered)
 	proximity_area.body_exited.connect(on_proximity_area_body_exited)
 	# use the proximity area to get the player's position
 	var bodies_in_proximity = proximity_area.get_overlapping_bodies()
@@ -25,31 +23,22 @@ func Enter():
 	pass
 
 func Update(_delta: float) -> void:
-	if npc && player:
-		move_direction = (npc.global_position - player.global_position).normalized()
+	if enemy && player:
+		move_direction = (player.global_position - enemy.global_position).normalized()
 	pass
 
 func Physics_Update(_delta: float):
-	if npc:
-		npc.velocity = move_speed * move_direction
+	if enemy:
+		enemy.velocity = move_speed * move_direction
 	pass
 
 # Class Specific Functions
 
 
 # Signals
-func on_infection_area_body_entered(body:Node2D):
-	if body is Player:
-		print("Transitioning to infected state")
-		Transitioned.emit(self, "NpcInfected")
-		# The NPC is infected, so we no longer need to listen to the infection or proximity area signal
-		infection_area.body_entered.disconnect(on_infection_area_body_entered)
-		proximity_area.body_exited.disconnect(on_proximity_area_body_exited)
 
 func on_proximity_area_body_exited(body:Node2D):
 	if body is Player:
-		print("Transitioning to idle state")
-		Transitioned.emit(self, "NpcIdle")
+		Transitioned.emit(self, "EnemyIdle")
 		# The NPC is no longer fleeing, so we no longer need to listen to the proximity area signal
-		infection_area.body_entered.disconnect(on_infection_area_body_entered)
 		proximity_area.body_exited.disconnect(on_proximity_area_body_exited)
