3c3fd9c5268487f232471eb1e2a41b63ffb4e15b
Peachy3D <alex.mortimerr@gmail.com>
2025-01-18 16:17:45 +0000

    enemynpc (#10)

--- project-virus/scenes/npcs/scripts/states/NpcInfected.gd
+++ project-virus/scenes/npcs/scripts/states/NpcInfected.gd
@@ -13,12 +13,12 @@ func randomize_wonder():
 	wonder_time = randf_range(1, 3)
 
 func Enter():
-    # Duplicate the shader material for unique instance
+	# Duplicate the shader material for unique instance
 	var original_material : ShaderMaterial = npc_sprite.material
 	var new_material : ShaderMaterial = original_material.duplicate()
 	npc_sprite.material = new_material
-    
-    # Enabling shader and particles for infected visuals
+	
+	# Enabling shader and particles for infected visuals
 	new_material.set_shader_parameter("shader_enabled", true)
 	var spore_particles: CPUParticles2D = npc_sprite.get_children()[0]
 	spore_particles.visible = true
@@ -33,4 +33,3 @@ func Update(delta: float) -> void:
 func Physics_Update(_delta: float):
 	if npc:
 		npc.velocity = move_direction * move_speed
-
--- project-virus/scenes/player/scripts/player.gd
+++ project-virus/scenes/player/scripts/player.gd
@@ -4,12 +4,12 @@ class_name Player
 @export var SPEED: float = 300.0
 
 func _physics_process(delta):
-    # Get the input direction and handle the movement.
-    var direction = Vector2(
-        Input.get_axis("ui_left", "ui_right"),
-        Input.get_axis("ui_up", "ui_down")
-    ).normalized()
+	# Get the input direction and handle the movement.
+	var direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	).normalized()
 
-    velocity = direction * SPEED
+	velocity = direction * SPEED
 
-    move_and_slide()
+	move_and_slide()
