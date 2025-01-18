6f640e914d0ff2978a8317c574ec29ccbbc2482e
Ryan Barker <35574294+Ryanbarker0@users.noreply.github.com>
2025-01-18 18:54:03 +0000

    Updated player model + added support for WASD control (#11)

--- project-virus/scenes/player/scripts/player.gd
+++ project-virus/scenes/player/scripts/player.gd
@@ -3,11 +3,11 @@ class_name Player
 
 @export var SPEED: float = 300.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Get the input direction and handle the movement.
 	var direction = Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
 	).normalized()
 
 	velocity = direction * SPEED
