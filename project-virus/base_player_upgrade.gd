c65755a3f8e8c5647f70d0941e30b81690f6880a
Emphasition <adamgaughan1@gmail.com>
2025-01-21 22:12:55 +0000

    Added speed upgrade (#28)

---
+++ project-virus/base_player_upgrade.gd
@@ -0,0 +1,15 @@
+class_name BasePlayerUpgrade
+extends Resource
+
+
+@export var texture : Texture2D = preload("res://scenes/levels/assets/items/animated_sprout_48x48.png")
+@export var upgrade_text : String = "Speed"
+
+
+func apply_upgrade(player: Player):
+	
+	pass
+
+func get_player_stats(player: Player) -> StatsComponent:
+	return player.get_node("StatsComponent")
+	
--- project-virus/scenes/player/scripts/player.gd
+++ project-virus/scenes/player/scripts/player.gd
@@ -3,6 +3,10 @@ class_name Player
 
 @export var stats_component: StatsComponent
 
+var upgrades: Array[BasePlayerUpgrade] = []
+@onready var current_upgrade_amount: int = 0
+
+
 func _physics_process(_delta):
 	# Get the input direction and handle the movement.
 	var direction = Vector2(
@@ -11,5 +15,7 @@ func _physics_process(_delta):
 	).normalized()
 
 	velocity = direction * stats_component.move_speed
-
+	if upgrades.size() != current_upgrade_amount:
+		current_upgrade_amount = upgrades.size()
+		upgrades[upgrades.size() - 1].apply_upgrade(self)
 	move_and_slide()
