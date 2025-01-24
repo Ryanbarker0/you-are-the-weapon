a3b737adb28837291e6a42becca725cfc2e0bec4
Ryan <ryangeorgebarker0@gmail.com>
2025-01-24 14:47:36 +0000

    update logic for adding new rotating projectiles

--- project-virus/scenes/player/scripts/rotating_persistent_projectile.gd
+++ project-virus/scenes/player/scripts/rotating_persistent_projectile.gd
@@ -10,8 +10,12 @@ var orbit_angle: float = 0.0
 	set(value):
 		orbit_radius = value
 
-func initialise_behaviour(persistent_projectile: PersistentProjectile):
-	orbit_angle = (persistent_projectile.global_position - persistent_projectile.parent.global_position).angle()
+func initialise_behaviour(persistent_projectile: PersistentProjectile, projectiles: Array[PersistentProjectile]):
+	if not projectiles.has(persistent_projectile):
+		projectiles.append(persistent_projectile)
+		
+	# Recalculate the positions of all projectiles
+	_recalculate_projectile_positions(projectiles)
 
 func apply_behaviour(persistent_projectile: PersistentProjectile, delta: float):
 	# Update the orbit angle
@@ -20,3 +24,23 @@ func apply_behaviour(persistent_projectile: PersistentProjectile, delta: float):
 	persistent_projectile.global_position = persistent_projectile.parent.global_position + Vector2(cos(orbit_angle), sin(orbit_angle)) * orbit_radius
 	# Optional: Update the rotation of the object (e.g., to face the orbit direction)
 	persistent_projectile.rotation = orbit_angle
+
+
+func _recalculate_projectile_positions(projectiles: Array[PersistentProjectile]):
+	# Get the total number of projectiles
+	var total_projectiles = projectiles.size()
+	if total_projectiles == 0:
+		return
+
+	# Recalculate the position of each projectile
+	for i in range(total_projectiles):
+		var angle = i * (2.0 * PI / total_projectiles)
+		var projectile = projectiles[i]
+
+		# Set the projectile's position based on the calculated angle
+		projectile.global_position = projectile.parent.global_position + Vector2(cos(angle), sin(angle)) * orbit_radius
+
+		# Update the orbit_angle for each projectile
+		var behaviour = projectile.behaviour_controller
+		if behaviour is RotatingPersistentProjectileBehaviour:
+			behaviour.orbit_angle = angle
