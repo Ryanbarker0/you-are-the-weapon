c400a04ca8525314cd60f2027626c51554cbed6e
Ryan <ryangeorgebarker0@gmail.com>
2025-01-25 22:49:28 +0000

    added volume sliders

--- project-virus/scenes/levels/scripts/npc_spawner.gd
+++ project-virus/scenes/levels/scripts/npc_spawner.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var BasicNpcScene: PackedScene
 @export var BasicEnemyScene: PackedScene
 
+@export var game_over_screen: Control
+
 var margin = 150
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 
@@ -23,6 +25,12 @@ func _ready():
 	basic_enemy_spawn_timer.timeout.connect(handle_spawn.bind(BasicEnemyScene, basic_enemy_spawn_timer))
 	# Add more entity timers here for additional entity types
 
+func _process(delta):
+	if game_over_screen.visible:
+		game_timer.stop()
+		basic_npc_spawn_timer.stop()
+		basic_enemy_spawn_timer.stop()
+
 func handle_spawn(entity_scene: PackedScene, timer: Timer):
 	spawner_component.scene = entity_scene
 	
@@ -31,16 +39,16 @@ func handle_spawn(entity_scene: PackedScene, timer: Timer):
 	var viewport_size = get_viewport_rect().size
 	var spawn_position = Vector2()
 
-    # Determine spawn position outside the camera window
+	# Determine spawn position outside the camera window
 	if randf() < 0.5:
-        # Spawn on the left or right side
+		# Spawn on the left or right side
 		if randf() < 0.5:
 			spawn_position.x = camera_position.x - viewport_size.x / 2 - margin
 		else:
 			spawn_position.x = camera_position.x + viewport_size.x / 2 + margin
 			spawn_position.y = randf_range(camera_position.y - viewport_size.y / 2 - margin, camera_position.y + viewport_size.y / 2 + margin)
 	else:
-        # Spawn on the top or bottom side
+		# Spawn on the top or bottom side
 		spawn_position.x = randf_range(camera_position.x - viewport_size.x / 2 - margin, camera_position.x + viewport_size.x / 2 + margin)
 		if randf() < 0.5:
 			spawn_position.y = camera_position.y - viewport_size.y / 2 - margin
