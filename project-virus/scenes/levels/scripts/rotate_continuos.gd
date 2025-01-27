d58d31e19ddbc106d1013fbe4e82be1f23d5996d
Ryan <ryangeorgebarker0@gmail.com>
2025-01-27 15:47:40 +0000

    change the upgrade item sprite

--- project-virus/scenes/levels/scripts/rotate_continuos.gd
+++ project-virus/scenes/levels/scripts/rotate_continuos.gd
@@ -1,8 +1,22 @@
 extends Sprite2D
 
-var _time := 0.0
-@export var speed := 1.0
+var tween: Tween
 
-func _process(delta):
-	_time = wrapf(_time + delta * speed, -PI,PI)
-	rotation = _time
+func _ready():
+	animate_sprite()
+
+func _restart_tween():
+	animate_sprite()	
+
+func animate_sprite():
+	tween = get_tree().create_tween()
+	tween.bind_node(self)
+	tween.set_loops(-1)
+
+	# Bobbing animation (up and down)
+	tween.tween_property(self, "position:y", position.y - 5, 0.2) \
+		.set_trans(Tween.TRANS_SINE) \
+		.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "position:y", position.y + 5, 0.2) \
+		.set_trans(Tween.TRANS_SINE) \
+		.set_ease(Tween.EASE_IN_OUT)
