99dd1b8301395412141b021fbc04eba6a4033cc5
Ryan Barker <35574294+Ryanbarker0@users.noreply.github.com>
2025-01-30 21:29:14 +0000

    Small update to health bar visibility (#54)

--- project-virus/scenes/ui/scripts/health_bar.gd
+++ project-virus/scenes/ui/scripts/health_bar.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var player:Player
 @onready var progress_bar: ProgressBar = $ProgressBar
+@onready var progress_bar_text: TextureProgressBar = $TextureProgressBar
 
 var playerstats: StatsComponent
 # Called when the node enters the scene tree for the first time.
@@ -13,8 +14,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if is_instance_valid(player):
 		progress_bar.value = playerstats.health
+		progress_bar_text.value = playerstats.health
 	else:
 		progress_bar.value = 0
+		progress_bar_text.value = 0
 
 	if progress_bar.value == 0 :
 		modulate = Color(0.80, 0.20, 0.12)
+		
